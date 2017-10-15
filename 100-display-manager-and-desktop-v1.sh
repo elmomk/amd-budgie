f94d86a82b891044add6d06f5aa5e321b8dc22d1
Erik Dubois <erik.dubois@gmail.com>
2017-10-15 13:00:39 +0200

    update

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -3,6 +3,7 @@ sudo pacman -Syyu
 sudo pacman -S gdm --noconfirm --needed
 sudo pacman -S gnome --noconfirm --needed
 #sudo pacman -S budgie-desktop
-packer budgie-desktop-git --noedit --noconfirm
+#packer budgie-desktop-git --noedit --noconfirm
+yaourt budgie-desktop-git --noconfirm
 sudo systemctl enable gdm.service -f
 sudo systemctl set-default graphical.target
