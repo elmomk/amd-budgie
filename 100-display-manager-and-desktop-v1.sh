106e26edb56be8d6f2658ef82efd2d493a980a43
Erik Dubois <erik.dubois@gmail.com>
2017-10-15 09:43:56 +0200

    update

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -1,5 +1,6 @@
 sudo pacman -Syyu
 sudo pacman -S gdm
-sudo pacman -S gnome gnome-extra
+sudo pacman -S gnome
+sudo pacman -S budgie-desktop
 sudo systemctl enable gdm.service -f
 sudo systemctl set-default graphical.target
