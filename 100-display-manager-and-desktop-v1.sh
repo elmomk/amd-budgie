76e3b0e2116038e9bcfd40e7e2cfb6b5794eed39
Erik Dubois <erik.dubois@gmail.com>
2017-10-15 17:04:18 +0200

    extra content

--- 100-display-manager-and-desktop-v1.sh
+++
@@ -1,9 +0,0 @@
-# use mirror if you have a slow connection
-sudo pacman -Syyu
-sudo pacman -S gdm --noconfirm --needed
-sudo pacman -S gnome --noconfirm --needed
-#sudo pacman -S budgie-desktop
-#packer budgie-desktop-git --noedit --noconfirm
-yaourt budgie-desktop-git --noconfirm
-sudo systemctl enable gdm.service -f
-sudo systemctl set-default graphical.target
---
+++ 100-display-manager-gdm-and-budgie-git-desktop-v1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+# Website	:	http://www.archmerge.com
+# Website	:	http://www.archmerged.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+# use mirror if you have a slow connection
+sudo pacman -Syyu
+sudo pacman -S gdm --noconfirm --needed
+sudo pacman -S gnome --noconfirm --needed
+#sudo pacman -S budgie-desktop
+#packer budgie-desktop-git --noedit --noconfirm
+yaourt budgie-desktop-git --noconfirm
+sudo systemctl enable gdm.service -f
+sudo systemctl set-default graphical.target
