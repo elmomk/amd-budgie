5fc22dca0f3ff3d0ecfb3198332d70bb15392b71
Arch MergeD <archmerged@gmail.com>
2017-10-20 14:25:56 +0200

    update printer script foomatic

--- 130-install-printers-v2.sh
+++ 130-install-printers-v2.sh
@@ -11,12 +11,18 @@ set -e
 #
 ##################################################################################################################
 
+sudo pacman -S --noconfirm --needed cups cups-pdf 
 
+sudo pacman -S foomatic-db-engine --noconfirm --needed
+sudo pacman -S foomatic-db foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds --noconfirm --needed
+sudo pacman -S ghostscript gsfonts gutenprint --noconfirm --needed
+sudo pacman -S libcups --noconfirm --needed
+sudo pacman -S hplip --noconfirm --needed
+sudo pacman -S system-config-printer --noconfirm --needed
 
-sudo pacman -S cups cups-pdf ghostscript gsfonts libcups hplip system-config-printer --noconfirm --needed
-systemctl enable org.cups.cupsd.service
-systemctl start org.cups.cupsd.service
+sudo systemctl enable org.cups.cupsd.service
 
+echo "now reboot"
 
 echo "################################################################"
 echo "#########   printer management software installed     ##########"
